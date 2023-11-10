b2aa20d27be181c67c7a2664630aec20adcf7af9
Buddi Navaneeth Kumar <35196622+Nava308@users.noreply.github.com>
2023-11-10 14:37:13 -0600

    Changed the server from 3000 to 3001

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("Server running on http://localhost:3000/");
 });
 
